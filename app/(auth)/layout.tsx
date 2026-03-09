c56262228cf3d42172a4fac0195c0539a94807a0
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-03-09 18:11:23 +0100

    impelement auth and storage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
+import { isAuthenticated } from '@/lib/actions/auth.action';
 import {ReactNode} from 'react'
+import { redirect } from 'next/navigation'
 
+const AuthLayout = async ({ children } : { children: React.ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
 
-const AuthLayout = ({ children } : { children: React.ReactNode }) => {
+  if(isUserAuthenticated) redirect("/");
   return (
     <div className='auth-layout'>{children}</div>
   )
